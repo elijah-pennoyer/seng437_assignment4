1e7312a253bd98c1f0c139653ee1225f1a3de8dc
wongna <natalie.wong@ucalgary.ca>
2017-03-16 10:18:20 -0600

    Resolved expand.

--- src/org/jfree/data/Range.java
+++ src/org/jfree/data/Range.java
@@ -60,6 +60,7 @@
 package org.jfree.data;
 
 import java.io.Serializable;
+import java.security.InvalidParameterException;
 
 /**
  * Represents an immutable range of values.
@@ -254,7 +255,7 @@ public strictfp class Range implements Serializable {
     public static Range expand(Range range, 
                                double lowerMargin, double upperMargin) {
         if (range == null) {
-            throw new IllegalArgumentException("Null 'range' argument.");   
+            throw new InvalidParameterException("Null 'range' argument.");   
         }
         double length = range.getLength();
         double lower = length * lowerMargin;
